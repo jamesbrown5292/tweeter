f15f0e259e80ddad416c2d2bd8b1d68d9efbf233
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-09-10 14:01:46 -0700

    show form subission on clicking write tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,27 +5,38 @@
  */
 /* GO back and append jQuery lines one at a time */
 $(() => {
+  $('.alert-messaging').hide();
+  $('.arrowicon').effect('bounce', "fast");
+
   let tweetIdNum = 0;
   const createTweetElement = (tweetObject) => {
-    const $tweet = $(` <article id="${tweetIdNum}">
-    <header>
-      <div class="username-with-icon">
-        <i class="fas fa-user-astronaut"></i>
-        <p>${tweetObject.user.name}</p>
-      </div>
-      <p class="hide">${tweetObject.user.handle}</p>
-    </header>
-    `)
-    $tweet.append($("<p>").addClass("tweet-article-body").text(tweetObject.content.text));
-    $tweet.append(`<footer>
-      <p>${tweetObject.created_at}</p>
-      <div class="like-fav-retweet-icons">
-        <i class="fas fa-heart"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-flag"></i>
-      </div>
-    </footer>
-  </article>`);
+
+    const escape =  function(str) {
+      let div = document.createElement('div');
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    }
+
+    const $tweet = $(` <article>
+
+          <header>
+            <div class="username-with-icon">
+              <i class="fas fa-user-astronaut"></i>
+              <p>James</p>
+            </div>
+            <p class="hide">${tweetObject.user.handle}</p>
+          </header>
+
+          <p class="tweet-article-body">${escape(tweetObject.content.text)}</p>
+
+          <footer>
+            <p>${tweetObject.created_at}</p>
+            <div class="like-fav-retweet-icons">
+              <i class="fas fa-heart"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-flag"></i>
+            </div>
+          </footer>`)
 
     return $tweet;
   };
@@ -33,9 +44,6 @@ $(() => {
   const $allTweetsSection = $('.all-users-tweets');
 
   const renderTweets = (tweets) => {
-    //loop through the tweets
-    //calls createElement on each tweet
-    //takes the value and appends to container
     $allTweetsSection.empty();
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
@@ -52,42 +60,60 @@ $(() => {
         console.log(error, "We couldn't find any tweets");
       });
   };
-  loadTweets();
-  const $postTweetForm = $('.tweet-box'); //this class name is confusing. 
+  loadTweets(); 
 
+
+  $('#new-tweet').hide();
+    
+  const $composeTweetBoxSection = $('.new-tweet');
+  $('.write-tweet-button-container').on('click', function() {
+    $composeTweetBoxSection.slideDown("2000");
+  });
+
+
+  const $postTweetForm = $('.tweet-box'); //this class name is confusing. 
   $postTweetForm.on('submit', function(event) {
     event.preventDefault();
+    $('.alert-messaging').hide();
     const $textarea = $('textarea');
     const $tweetContent = $textarea.val();
     const $writeTweetBox = $('.tweet-box')
-    const $alertBox = $('<div class="alert-messaging"></div>')
-    if (!$tweetContent.length) {
+    const $alertBoxContainer = $('.alert-box-container');
+    const $alertBox = $('.alert-messaging');
+    const $newTweetBox = $('.new-tweet')
+    
+    const removeAlertElements = () => {
       $('.alert-messaging').remove();
-      $writeTweetBox.prepend($alertBox);
-      $alertBox.text('You can\'t post an empty tweet. That would be silly.')
-    } else if ($tweetContent === null) {
-      $('.alert-messaging').remove();
-      $writeTweetBox.prepend($alertBox);
-      $alertBox.text('You can\'t post an empty tweet. That would be silly.')
+      $('.alert-box-container').remove();
+    };
+
+    const insertAlertElements = (alertMessage) => {
+      $alertBox.text(alertMessage).slideDown("2000");
+      //$alertBoxContainer.slideDown("2000")
+    };
+    
+    if (!$tweetContent.length || $tweetContent === null) {
+      const alertMessage = '\u26A0 ' +  'You can\'t post an empty tweet. That would be silly.' + ' \u26A0'
+      insertAlertElements(alertMessage);
+
+
     } else if ($tweetContent.length > 140) {
-      $('.alert-messaging').remove();
-      $writeTweetBox.prepend($alertBox);
-      $alertBox.text('Your tweet is too long. Keep it to 140 characters or less!')
+      const alertMessage = '\u26A0 ' + 'Your tweet is too long. Keep it to 140 characters or less!' + ' \u26A0'
+      //removeAlertElements();
+      insertAlertElements(alertMessage)
     } else {
-      $('.alert-messaging').remove();
       const serializedData = $(this).serialize();
+      //removeAlertElements();
+
       $.post('/tweets/', serializedData)
         .then((response) => {
-          console.log(response);
           $allTweetsSection.empty();
           $textarea.val('');
           loadTweets();
         });
-    }
+    };
+
   });
-
-
-
 });
 
 //Leave high level comments for the endpoint behaviours, don't detail the code. 
