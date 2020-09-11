257c5d75d01c165715e42fe6f3aaa0733a5c86ff
jamesbrown5292 <jamesbrown5292@gmail.com>
2020-09-10 23:33:55 -0700

    refactoring client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,14 @@
 
 
 $(() => {
+  //Hide any existing alerts and text input and initiate icon bounce
   $('.alert-messaging').hide();
   $('.arrowicon').effect('bounce', "fast");
+  $('#new-tweet').hide();
 
-  let tweetIdNum = 0;
   const createTweetElement = (tweetObject) => {
 
+    //escape function to prevent XSS
     const escape =  function(str) {
       let div = document.createElement('div');
       div.appendChild(document.createTextNode(str));
@@ -21,7 +23,13 @@ $(() => {
       const hoursAgo = Math.floor(millisecondsAgo / 3600000);
       const yearsAgo = Math.floor(millisecondsAgo / 31536000000)
       if (daysAgo < 1) {
+        if (hoursAgo > 1) {
         return hoursAgo + " hours ago";
+        } else if (hoursAgo === 1) {
+          return hoursAgo + " hour ago";
+        } else {
+          return "Within the last hour"
+        }
       } else if (daysAgo === 1) {
         return daysAgo + " day ago";
       } else if (daysAgo >= 365 && daysAgo < 730) {
@@ -31,6 +39,7 @@ $(() => {
       }
     }
 
+    
     const $tweet = $(` <article>
           <header>
             <div class="username-with-icon">
@@ -39,9 +48,7 @@ $(() => {
             </div>
             <p class="hide">${tweetObject.user.handle}</p>
           </header>
-
           <p class="tweet-article-body">${escape(tweetObject.content.text)}</p>
-
           <footer>
             <p>${convertTime(tweetObject.created_at)}</p>
             <div class="like-fav-retweet-icons">
@@ -54,6 +61,7 @@ $(() => {
   };
 
   const $allTweetsSection = $('.all-users-tweets');
+  const $textInputField = $('textarea');
 
   const renderTweets = (tweets) => {
     $allTweetsSection.empty();
@@ -72,16 +80,16 @@ $(() => {
         console.log(error, "We couldn't find any tweets");
       });
   };
+
   loadTweets(); 
 
 
-  $('#new-tweet').hide();
-  const $textarea = $('textarea');
+  
     
   const $composeTweetBoxSection = $('.new-tweet');
   $('.write-tweet-button-container').on('click', function() {
     $composeTweetBoxSection.slideDown("2000");
-    $textarea.focus();
+    $textInputField.focus();
   });
 
 
@@ -89,17 +97,12 @@ $(() => {
   $postTweetForm.on('submit', function(event) {
     event.preventDefault();
     $('.alert-messaging').hide();
-    const $tweetContent = $textarea.val();
+    const $tweetContent = $textInputField.val();
     const $writeTweetBox = $('.tweet-box')
     const $alertBoxContainer = $('.alert-box-container');
     const $alertBox = $('.alert-messaging');
     const $newTweetBox = $('.new-tweet')
     
-    const removeAlertElements = () => {
-      $('.alert-messaging').remove();
-      $('.alert-box-container').remove();
-    };
-
     const insertAlertElements = (alertMessage) => {
       $alertBox.text(alertMessage).slideDown("2000");
       //$alertBoxContainer.slideDown("2000")
@@ -121,7 +124,7 @@ $(() => {
       $.post('/tweets/', serializedData)
         .then((response) => {
           $allTweetsSection.empty();
-          $textarea.val('');
+          $textInputField.val('');
           loadTweets();
         });
     };
@@ -129,4 +132,3 @@ $(() => {
   });
 });
 
-//Leave high level comments for the endpoint behaviours, don't detail the code. 
